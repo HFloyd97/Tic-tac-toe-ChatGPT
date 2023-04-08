e3727010fe6a73052954ed3a175a017077b72b34
hjfloyd <77729147+hjfloyd@users.noreply.github.com>
2023-04-09 00:10:44 +0100

    update

--- script.js
+++ script.js
@@ -1,45 +1,78 @@
-const cells = document.querySelectorAll('.cell');
-const status = document.querySelector('#status');
-const reset = document.querySelector('#reset');
-
-let currentPlayer = 'X';
-let gameEnded = false;
-
-cells.forEach(cell => {
-  cell.addEventListener('click', () => {
-    if (gameEnded || cell.textContent !== '') {
-      return;
-    }
-    cell.textContent = currentPlayer;
-    checkGameStatus();
-    switchPlayer();
-  });
-});
-
-reset.addEventListener('click', () => {
-  cells.forEach(cell => {
-    cell.textContent = '';
-  });
-  currentPlayer = 'X';
-  gameEnded = false;
-  status.textContent = "Player 1's turn (X)";
-});
-
-function checkGameStatus() {
-  const winPatterns = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-    [0, 4, 8], [2, 4, 6] // diagonals
+const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
   ];
-  for (let pattern of winPatterns) {
-    const [a, b, c] = pattern;
-    if (cells[a].textContent === currentPlayer &&
-        cells[b].textContent === currentPlayer &&
-        cells[c].textContent === currentPlayer) {
-      status.textContent = `${currentPlayer} wins!`;
-      gameEnded = true;
-      return;
+  
+  let currentPlayer = 'X';
+  
+  function makeMove(row, col) {
+    if (board[row][col] === '') {
+      board[row][col] = currentPlayer;
+      updateBoard();
+      
+      if (checkWinner(row, col)) {
+        alert(currentPlayer + ' wins!');
+        resetBoard();
+      } else if (isBoardFull()) {
+        alert('It\'s a draw!');
+        resetBoard();
+      } else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      }
     }
   }
-  if (Array.from(cells).every(cell => cell.textContent !== '')) {
-   
+  
+  function updateBoard() {
+    const cells = document.querySelectorAll('.cell');
+    let i = 0;
+    for (const row of board) {
+      for (const cell of row) {
+        cells[i].textContent = cell;
+        i++;
+      }
+    }
+  }
+  
+  function checkWinner(row, col) {
+    // Check row
+    if (board[row][0] === currentPlayer && board[row][1] === currentPlayer && board[row][2] === currentPlayer) {
+      return true;
+    }
+    
+    // Check column
+    if (board[0][col] === currentPlayer && board[1][col] === currentPlayer && board[2][col] === currentPlayer) {
+      return true;
+    }
+    
+    // Check diagonals
+    if (board[0][0] === currentPlayer && board[1][1] === currentPlayer && board[2][2] === currentPlayer) {
+      return true;
+    }
+    
+    if (board[0][2] === currentPlayer && board[1][1] === currentPlayer && board[2][0] === currentPlayer) {
+      return true;
+    }
+    
+    return false;
+  }
+  
+  function isBoardFull() {
+    for (const row of board) {
+      for (const cell of row) {
+        if (cell === '') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  function resetBoard() {
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        board[row][col] = '';
+      }
+    }
+    updateBoard();
+  }
